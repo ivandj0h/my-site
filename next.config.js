e17b6c6d24b2f43352f733a88e692fff48945a1e
ivandjoh <ivandi.djoh@gmail.com>
2023-05-15 08:58:48 +0800

    image config & readme updated!

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+                pathname: '/ivandjoh/image/upload/v1684110803/ID_y93mbe.png',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
